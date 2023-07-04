d3554c39b76eb51598233bf255e4711c2587dac8
lovvtide <sbowman@satellite.earth>
2023-07-03 22:23:10 -0700

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    includeAssets: ['favicon.png'],
+    manifest: {
+      name: 'Satellite',
+      short_name: 'Satellite',
+      description: 'Nostr client',
+      icons: [
+        {
+          src: '/favicon.png',
+          sizes: '196x196',
+          type: 'image/png'
+        }
+      ]
+    },
+    theme_color: '#171819',
+    background_color: '#171819',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait'
+  })]
+});
