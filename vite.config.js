72e011ef97426247571222dd3a959bdd767749b5
lovvtide <sbowman@satellite.earth>
2023-07-09 09:06:56 -0700

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  //base: './',
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     selfDestroying: true,
